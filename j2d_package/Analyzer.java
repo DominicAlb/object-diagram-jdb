16e1a8a386601b4632e256d517a8867131ff98c5
Dominic Albers <120376667+DominicAlb@users.noreply.github.com>
2022-12-13 14:44:55 +0100

    Add files via upload

---
+++ j2d_package/Analyzer.java
@@ -0,0 +1,331 @@
+package j2d_package;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+/*
+ * 
+ * Finished 14.07.2022
+ * made by Dominic
+ * 
+ * Analyzes the class composition and creates schemes for the object information
+ * which you can use for example to create a history diagram or an object diagram
+ * 
+ */
+public class Analyzer {
+
+    BufferedReader br;
+    HashMap<String, String> classVars;
+    HashMap<String, String> primitiveVars;
+    HashMap<String, String> methods;
+    String main_file_name;
+    String dir;
+
+    public Analyzer(String dir, String main_file_name) {
+        this.dir = dir;
+        this.main_file_name = main_file_name;
+        classVars = new HashMap<String, String>();
+        primitiveVars = new HashMap<String, String>();
+        methods = new HashMap<String, String>();
+        try {
+            br = new BufferedReader(new FileReader(new File(dir + main_file_name + ".java")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // creates schemes for every var type used
+    public HashMap<String, String> getSchemesFromFile() throws IOException {
+        HashMap<String, String> schemes = new HashMap<String, String>();
+
+        //gets the currently known vars
+        getInitiatedVars(main_file_name);
+
+        for (String key : primitiveVars.keySet()) {
+            schemes.put(key, createPrimitiveVarScheme(key, primitiveVars.get(key)));
+        }
+        for (String key : classVars.keySet()) {
+            schemes.put(key, createClassScheme(key, classVars.get(key), getClassAttributes(classVars.get(key))));
+        }
+
+        return schemes;
+    }
+
+    // gets the info of a complex object by analizing its local java file in the package
+    public HashMap<String, String> getClassAttributes(String class_type) {
+
+        HashMap<String, String> attributes = new HashMap<String, String>();
+        String m = ",methods={";
+        try {
+            File f = new File(getClassLocation(class_type));
+            BufferedReader bReader = new BufferedReader(new FileReader(f));
+            String line;
+            String[] parts;
+            boolean inClass = false;
+            int braces = 0;
+
+            // read though each line of code
+            while ((line = bReader.readLine()) != null) {
+                char isVisible = '+';
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == '{') {
+                        braces++;
+                    }
+                    if (line.charAt(i) == '}') {
+                        braces--;
+                    }
+                }
+
+                if (inClass && braces == 0) {
+                    inClass = false;
+                }
+
+                // select the symbol based of the accessibility
+                if (line.trim().startsWith("private"))
+                    isVisible = '-';
+                if (line.trim().startsWith("protected"))
+                    isVisible = '#';
+                line = line.replace("public", "").replace("private", "").replace("protected", "").trim();
+                parts = line.split(" ");
+
+                if (parts == null) {
+                    continue;
+                }
+                if (parts.length < 2) {
+                    continue;
+                }
+                if (parts[0] == null || parts[1] == null) {
+                    continue;
+                }
+
+                if (parts[0].equals("class") && parts[1].equals(class_type)) {
+                    inClass = true;
+                }
+                if (inClass) {
+                    //braces == 1 -> currently in class but not in a method
+                    if (braces == 1) {
+                        if (Character.isUpperCase(line.charAt(0))) {
+                            // check if it is a method else it is a complex var because of uppercase char
+                            if (!parts[1].contains("("))
+                                //put var into attributes list
+                                attributes.put(parts[1].replace("=", "").replace(";", ""), isVisible + " " + parts[0]);
+                            else if (parts[1].contains("(")) {
+                                // analyze the method and create the part of a scheme
+                                String retType, name, args;
+                                name = parts[1].split("\\(")[0].trim();
+                                retType = parts[0];
+                                if (line.contains("()"))
+                                    args = "";
+                                else
+                                    args = parts[1].split("\\(")[1] + " " + parts[2].split("\\)")[0];
+                                m = m + "mname:" + isVisible + " " + name + "-mtype:" + retType + "-margs:" + args
+                                        + ";";
+                            }
+                        // if lower case char check if it a primitive var
+                        } else if (isPrimitive(parts[0])) {
+                            if (!parts[1].contains("("))
+                                attributes.put(parts[1].replace("=", "").replace(";", ""), isVisible + " " + parts[0]);
+                        // because the public/private/protected is already processed the line can start with void, which is neither a complex var type nor a primitive type
+                        } else if (line.startsWith("void")) {
+                            String retType, name, args;
+                            String[] mparts = parts[1].split("\\(");
+                            name = mparts[0].trim();
+                            retType = parts[0];
+                            if (line.contains("()"))
+                                args = "";
+                            else
+                                args = parts[1].split("\\(")[1] + " " + parts[2].split("\\)")[0];
+                            m = m + "mname:" + isVisible + " " + name + "-mtype:" + retType + "-margs:" + args + ";";
+                        }
+                    //braces == 2 -> currently in a method
+                    } else if (braces == 2) {
+                        if (Character.isUpperCase(line.charAt(0))) {
+                            if (parts[1].contains("(")) {
+                                String retType, name, args;
+                                String[] mparts = parts[1].split("\\(");
+                                name = mparts[0].trim();
+                                retType = parts[0];
+                                if (line.contains("()"))
+                                    args = "";
+                                else
+                                    args = parts[1].split("\\(")[1] + " " + parts[2].split("\\)")[0];
+                                m = m + "mname:" + isVisible + " " + name + "-mtype:" + retType + "-margs:" + args
+                                        + ";";
+                            }
+                        } else if (line.startsWith("void")) {
+                            String retType, name, args;
+                            String[] mparts = parts[1].split("\\(");
+                            name = mparts[0].trim();
+                            retType = parts[0];
+                            if (line.contains("()"))
+                                args = "";
+                            else
+                                args = parts[1].split("\\(")[1] + " " + parts[2].split("\\)")[0];
+                            m = m + "mname:" + isVisible + " " + name + "-mtype:" + retType + "-margs:" + args
+                                    + ";";
+                        }
+                    }
+                }
+            }
+
+            bReader.close();
+            methods.put(class_type, m + "}");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return attributes;
+    }
+
+    // returns the full class path
+    public String getClassLocation(String class_name) {
+        String location = dir + class_name + ".java";
+        File f = new File(location);
+        if (f.exists() && !f.isDirectory()) {
+            return location;
+        } else {
+            return dir + main_file_name + ".java";
+        }
+    }
+
+    // takes the data and creates a scheme of a complex object
+    //structure: "name=*STRING*,id=*STRING*,type=*STRING*,attr={id:*STRING*-type:*STRING*-value:{*STRING*}},methods={mname:*STRING*-mtype:*STRING*-margs:*STRING*}"
+    //splitable because of different divider eg.: attributes divided by '-' and assigned by ':' 
+    public String createClassScheme(String class_name, String class_type, HashMap<String, String> idType) {
+        String attr = "";
+        int index = 1;
+        for (String key : idType.keySet()) {
+            attr = attr + "id:" + key + "-type:" + idType.get(key) + "-value:{" + index + "};";
+            index++;
+        }
+        String ret = "name=" + class_name + ",id={0},type=" + class_type + ",attr={" + attr + "}"
+                + methods.get(class_type);
+
+        return ret;
+    }
+
+    // simple primitive var scheme structure: "name=*STRING*,type=*STRING*,value={0}""
+    public String createPrimitiveVarScheme(String var_name, String var_type) {
+        return "name=" + var_name + ",type=" + var_type + ",value={0}";
+    }
+
+    //get the currently initiated vars of the file
+    public void getInitiatedVars(String main_class) {
+        try {
+            String line;
+            String[] parts;
+            boolean inClass = false, inMain = false;
+            int braces = 0;
+
+            //read through each line of the file and get each var from main()
+            while ((line = br.readLine()) != null) {
+                // keep track of the opened braces, if amount = 2 -> in a method, if amount = 1 -> in a class
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == '{') {
+                        braces++;
+                    }
+                    if (line.charAt(i) == '}') {
+                        braces--;
+                    }
+                }
+                if (inClass && braces == 0) {
+                    inClass = false;
+                }
+                if (inClass && braces <= 1) {
+                    inMain = false;
+                }
+                if (line.contains("public static void main"))
+                    inMain = true;
+                line = line.replace("public", "").replace("private", "").replace("protected", "").trim();
+                parts = line.split(" ");
+
+                if (parts == null) {
+                    continue;
+                }
+                if (parts.length < 2) {
+                    continue;
+                }
+                if (parts[0] == null || parts[1] == null) {
+                    continue;
+                }
+
+                if (parts[0].equals("class") && parts[1].equals(main_class)) {
+                    inClass = true;
+                }
+
+                if (inClass) {
+                    if (inMain) {
+                        if (Character.isUpperCase(line.charAt(0))) {
+                            if (parts[0].equals("String")) {
+                                primitiveVars.put(parts[1].replace("=", "").replace(";", ""), parts[0]);
+                            } else {
+                                classVars.put(parts[1].replace("=", "").replace(";", ""), parts[0]);
+                            }
+
+                        } else if (isPrimitive(parts[0])) {
+                            primitiveVars.put(parts[1].replace("=", "").replace(";", ""), parts[0]);
+                        }
+                    }
+
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // getter/setter etc:
+
+    public HashMap<String, String> getClassVars() {
+        return this.classVars;
+    }
+
+    public HashMap<String, String> getPrimitiveVars() {
+        return this.primitiveVars;
+    }
+
+    private boolean isPrimitive(String s) {
+        switch (s) {
+            case "int":
+                return true;
+            case "byte":
+                return true;
+            case "short":
+                return true;
+            case "long":
+                return true;
+            case "float":
+                return true;
+            case "double":
+                return true;
+            case "boolean":
+                return true;
+            case "char":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public void setDir(String new_dir) {
+        this.dir = new_dir;
+    }
+
+    public void setMainFile(String mainFile) {
+        this.main_file_name = mainFile;
+    }
+
+    public String getDir() {
+        return this.dir;
+    }
+
+    public String getMainFile() {
+        return this.main_file_name;
+    }
+
+}
---
+++ j2d_package/j2dot.java
@@ -0,0 +1,394 @@
+package j2d_package;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.lang.ProcessBuilder;
+
+public class j2dot {
+
+  Scanner m_jdb_out;
+  PrintStream m_jdb_in;
+  static String m_mainclass;
+  static String dir;
+  static String pack;
+  Process p_jdb;
+  Process p_java;
+  ProcessBuilder proc_java;
+  ProcessBuilder proc_jdb;
+  private boolean isFinished = false;
+  FileWriter fw;
+  XMLHandler xmlHandler;
+  List<List<String>> xmlInfo;
+  Analyzer analyzer;
+  HashMap<String, String> schemes;
+
+  /*
+   * made by Dominic
+   * 
+   * In progress
+   * last update: 14.07.2022
+   * 
+   * Analyzes a java programm and creates a fitting xml diagram with the object
+   * composition
+   */
+
+  j2dot() {
+    try {
+      fw = new FileWriter(dir + "text.txt");
+      ProcessBuilder compJavaFile;
+      // compile target File
+      if (pack != null) {
+        compJavaFile = new ProcessBuilder("cmd.exe", "/c", "cd", dir , "&", "javac", "-g",
+            pack + "\\" + m_mainclass + ".java");
+        // System.out.println("javac -g " + pack + "\\" + m_mainclass + ".java");
+      } else {
+        compJavaFile = new ProcessBuilder("cmd.exe", "/c", "cd", dir, "&", "javac", "-g",
+            m_mainclass + ".java");
+        // System.out.println("javac -g " + m_mainclass + ".java");
+      }
+
+      compJavaFile.redirectErrorStream(true);
+      Process compProcess = compJavaFile.start();
+      compProcess.destroy();
+      compJavaFile = null;
+
+      if (pack != null) {
+        // build java process (Windows)
+        proc_java = new ProcessBuilder("cmd.exe", "/c", "cd", dir , "&", "java",
+            "-agentlib:jdwp=transport=dt_socket,address=localhost:8888,server=y,suspend=y", pack + "." + m_mainclass);
+        // System.out.println("java
+        // -agentlib:jdwp=transport=dt_socket,address=localhost:8888,server=y,suspend=y
+        // " + pack
+        // + "." + m_mainclass);
+      } else {
+        // build java process (Windows)
+        proc_java = new ProcessBuilder("cmd.exe", "/c", "cd", dir , "&","java",
+            "-agentlib:jdwp=transport=dt_socket,address=localhost:8888,server=y,suspend=y", m_mainclass);
+        // System.out.println(
+        // "java
+        // -agentlib:jdwp=transport=dt_socket,address=localhost:8888,server=y,suspend=y
+        // " + m_mainclass);
+      }
+      // build jdb process added to java process (Windows)
+      proc_jdb = new ProcessBuilder("jdb", "-connect", "com.sun.jdi.SocketAttach:hostname=localhost,port=8888");
+      proc_jdb.redirectErrorStream(true);
+
+      /*
+       * build java process (Linux)
+       * ProcessBuilder proc_java = new ProcessBuilder("java","-Xdebug",
+       * "-Xrunjdwp:transport=dt_socket,server=y,address=localhost:8080", "Figur" )
+       * 
+       * build jdb process added to java process (Linux)
+       * ProcessBuilder proc_jdb = new
+       * ProcessBuilder("jdb","-attach","localhost:8080");
+       */
+      if (pack != null) {
+        proc_java.directory(new File(dir + pack + "\\"));
+        proc_jdb.directory(new File(dir + pack + "\\"));
+        // System.out.println(dir + pack + "\\");
+      } else {
+        proc_java.directory(new File(dir));
+        proc_jdb.directory(new File(dir));
+        // System.out.println(dir);
+      }
+
+      // start both processes
+      p_java = proc_java.start(); // Process p_java = proc_java.start();
+      p_jdb = proc_jdb.start();
+
+      // Scanner
+      // to read jdb output
+      m_jdb_out = new Scanner(p_jdb.getInputStream()).useDelimiter("\\z|\\n");
+
+      // PrintStream
+      // to give jdb input commands
+      m_jdb_in = new PrintStream(p_jdb.getOutputStream());
+      System.out.println(pack);
+      if (pack != null) {
+        xmlHandler = new XMLHandler(dir + pack + "\\", "ObjectState");
+        analyzer = new Analyzer(dir + pack + "\\", m_mainclass);
+        // Analyzer analyzer = new Analyzer(dir + "Test" + "\\", m_mainclass);
+      } else {
+        xmlHandler = new XMLHandler(dir, "ObjectState");
+        analyzer = new Analyzer(dir, m_mainclass);
+      }
+      xmlInfo = new ArrayList<List<String>>();
+      schemes = analyzer.getSchemesFromFile();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+
+    }
+  }
+
+  public static void main(String[] args) {
+
+    JTextField xField = new JTextField(10);
+    JTextField pField = new JTextField(10);
+    JTextField yField = new JTextField(30);
+
+    JPanel myPanel = new JPanel();
+    myPanel.add(new JLabel("class:"));
+    myPanel.add(xField);
+    myPanel.add(Box.createHorizontalStrut(15));
+    myPanel.add(new JLabel("package:"));
+    myPanel.add(pField);
+    myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+    myPanel.add(new JLabel("dir:"));
+    myPanel.add(yField);
+
+    int result = JOptionPane.showConfirmDialog(null, myPanel,
+        "Please Enter class and path - eg: class: Test  package: <Empty>  dir: C:\\  or class: Test package: Test   dir: C:\\",
+        JOptionPane.OK_CANCEL_OPTION);
+    if (result == JOptionPane.OK_OPTION) {
+      m_mainclass = xField.getText().trim();
+      System.out.println(m_mainclass);
+      dir = yField.getText().trim();
+      if (!pField.getText().matches(" +"))
+        pack = pField.getText().trim();
+      else
+        pack = null;
+      System.out.println(pack);
+      dir = dir.replace("\\", File.separator) + File.separator;
+      j2dot j = new j2dot();
+
+      j.run();
+    }
+
+  }
+
+  void run() {
+    try {
+      // set break point in main classes main()-method
+      if (pack != null) {
+        jdb_exc("stop in " + pack + "." + m_mainclass + ".main");
+      } else {
+        jdb_exc("stop in " + m_mainclass + ".main");
+      }
+
+      jdb_exc("run");
+      jdb_getResponse();
+      Thread.sleep(50);
+      jdb_exc("thread 1");
+      while (true) {
+      
+        // prints here: BREAK by main
+        jdb_exc("next");
+        Thread.sleep(50);
+        String jdb_response = jdb_getResponse();
+
+        // if finished -> print it
+        if ((jdb_response) == null) {
+          System.out.println("BREAK by exception");
+          isFinished = true;
+        }
+        if ((jdb_response).contains("application exited")) {
+          System.out.println("exited");
+          isFinished = true;
+        }
+        if (isFinished) {
+          System.out.println("Now Finishing");
+          clean_up();
+          break;
+        }
+        jdb_exc("locals");
+        Thread.sleep(50);
+        String data = jdb_getResponse();
+        processData(data);
+
+      }
+      System.out.println("Write to XML");
+      xmlHandler.writeXML(xmlInfo);
+      xmlHandler.createGraphFromXML();
+    } catch (Exception e) {
+    }
+  }
+
+  // give jdb commands
+  // for info:
+  // https://docs.oracle.com/javase/7/docs/technotes/tools/windows/jdb.html
+  void jdb_exc(String p_cmd) {
+    m_jdb_in.println(p_cmd);
+    m_jdb_in.flush();
+  }
+
+  void clean_up() {
+    try {
+      p_jdb.destroy();
+      p_java.destroy();
+      m_jdb_out.close();
+      m_jdb_in.close();
+      proc_java = null;
+      proc_jdb = null;
+    } catch (Exception e) {
+    }
+  }
+
+  // prints response and returns if sort of check is needed
+  String jdb_getResponse() throws InterruptedException {
+    Thread.sleep(50);
+    String response = jdb_read();
+    System.out.println(response);
+    return response;
+  }
+
+  // reads, returns jdb response
+  String jdb_read() {
+    String ret = "";
+    while (true) {
+      if (m_jdb_out.hasNext()) {
+        String l = m_jdb_out.next();
+        ret += l + "\n";
+
+        if (l.matches("main\\[.*")) {
+          break;
+        }
+        if (l.contains("application exited")) {
+          System.out.println("exited");
+          isFinished = true;
+          break;
+        }
+        if(l.matches("ain\\[.*")) {
+          break;
+        }
+      } else
+        break;
+    }
+    return (ret);
+  }
+
+  public void processData(String data) throws InterruptedException {
+    List<String> dataInfo = new ArrayList<String>();
+    List<String> knownVars = getKnownVarsFromData(data);
+    String s;
+    for (String var : knownVars) {
+      String scheme = schemes.get(var);
+      String objName = var;
+      if (isPrimitiveScheme(scheme)) {
+        String value = getPrimitiveValue(objName);
+        s = scheme.replace("{0}", value.replace("\nmain[1]", "").trim());
+        dataInfo.add(s);
+      } else {
+        String id = getObjectId(objName);
+        HashMap<String, String> attrVs = getAttrValues(objName.trim());
+        s = scheme.replace("{0}", id);
+        String[] ids = getAttributeListInOrder(s);
+        for (int i = 0; i < ids.length; i++) {
+          String key = ids[i];
+          s = s.replace("{" + (int)(i+1) + "}", attrVs.get(key));
+        }
+        
+        dataInfo.add(s.trim());
+      }
+    }
+    xmlInfo.add(dataInfo);
+  }
+
+  public List<String> getKnownVarsFromData(String data) {
+    List<String> vars = new ArrayList<String>();
+    String[] used = data.split(":");
+    String[] lines = used[2].split("\n");
+    for (String line : lines) {
+      if (line.contains("=")) {
+        if (line.trim().contains(" "))
+          if (!line.contains("args"))
+            vars.add(line.split("=")[0].trim());
+      }
+    }
+    return vars;
+  }
+
+  public String[] getAttributeListInOrder(String scheme) {
+    String[] parts = scheme.replace("}", "").split(",");
+    String[] attrs = parts[3].split(";");
+    String[] array = new String[attrs.length];
+    int index = 0;
+    for (String attr : attrs) {
+      array[index] = attr.split("-")[0].split(":")[1];
+      index++;
+    }
+    return array;
+  }
+
+  public String getNameFromScheme(String scheme) {
+    String name = scheme.split(",")[0].split("=")[1];
+    return name;
+  }
+
+  public boolean isPrimitiveScheme(String scheme) {
+    if (scheme.contains("attr={")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public String getPrimitiveValue(String objName) throws InterruptedException {
+    jdb_exc("dump " + objName);
+    String resp = jdb_getResponse();
+    String[] temp = resp.split("=");
+    return temp[1].trim();
+  }
+
+
+  public HashMap<String, String> getAttrValues(String objName) throws InterruptedException {
+    HashMap<String, String> attrValues = new HashMap<String, String>();
+    jdb_exc("dump " + objName);
+    String resp = jdb_getResponse();
+    String[] temp2 = resp.split("\n");
+    String[] attribute;
+    for (String l : temp2) {
+      l = l.trim();
+      if (l.contains(":")) {
+        attribute = l.split(":");
+        if (attribute[1].contains("instance of")) {
+          String attrObj;
+          if(attribute[1].contains("[")) {
+            attrObj = getObjectIdsFromArray(objName + "." + attribute[0]);
+          }else {
+            attrObj = getObjectId(objName + "." + attribute[0]);
+          }
+          
+          attrValues.put(attribute[0].trim(), attrObj);
+        } else {
+          attrValues.put(attribute[0].trim(), attribute[1].trim());
+        }
+      }
+    }
+    return attrValues;
+  }
+
+  // get memory adresses from array
+  public String getObjectIdsFromArray(String objName) throws InterruptedException {
+    jdb_exc("print " + objName + ".length");
+    String length = jdb_getResponse().split("=")[1].replace("main[1]", "").trim();
+    int len = Integer.parseInt(length);
+    String ret = "[";
+    for(int i = 0; i < len; i++) {
+      ret = ret + getObjectId(objName+"["+i+"]") + ".";
+    }
+    ret = ret.substring(0, ret.length()-1) + "]";
+    return ret;
+  }
+
+  // get memory adress
+  public String getObjectId(String objName) throws InterruptedException {
+    jdb_exc("eval " + objName);
+    String[] idps = jdb_getResponse().split("\"");
+    String id = idps[1].split("@")[1];
+    return id;
+  }
+
+}
