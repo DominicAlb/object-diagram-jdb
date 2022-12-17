0c81e4213742abe63a58a683cf10d2da66ac95ca
Dominic Albers <dom.albers@gmail.com>
2022-12-17 15:39:25 +0100

    Update XMLHandler.java

--- j2d_package/XMLHandler.java
+++ j2d_package/XMLHandler.java
@@ -234,6 +234,183 @@ public class XMLHandler {
         fw.write(gc.getFinishedHTMLStringPart3());
         fw.flush();
     }
+    
+    public void createDotGraphFromXML() throws SAXException, IOException {
+        HashMap<String, List<String>> connections; 
+        HashMap<String, String> nameByID;
+        GraphCreator gc = new GraphCreator();
+        File xmlDoc = new File(dir + className + ".xml");
+        Document d = builder.parse(xmlDoc);
+        d.getDocumentElement().normalize();
+
+        // steps in code
+        NodeList stepList = d.getElementsByTagName("step");
+        String[] stepsDot = new String[stepList.getLength()];
+        for (int index1 = 0; index1 < stepList.getLength(); index1++) {
+            connections = new HashMap<>(30);
+            nameByID = new HashMap<>(15);
+            nameByID.put("null", "null");
+            gc.createDotNode("null");
+
+            Node step = stepList.item(index1);
+            if (step.getNodeType() == Node.ELEMENT_NODE) {
+                // Element stepElement = (Element) step;
+
+                // known Objects at that step
+                NodeList objectList = step.getChildNodes();
+
+                // assign object ids to their object names
+                for (int index2 = 0; index2 < objectList.getLength(); index2++) {
+                    Node object = objectList.item(index2);
+                    if (object.getNodeType() == Node.ELEMENT_NODE) {
+                        Element objectElement = (Element) object;
+                        String objName = objectElement.getAttribute("name");
+                        if (objectElement.hasAttribute("id")) {
+                            String objectID = objectElement.getAttribute("id");
+                            nameByID.put(objectID, objName);
+                        }
+                    }
+                }
+
+                // loop and create objects with connections
+                for (int index2 = 0; index2 < objectList.getLength(); index2++) {
+                    Node object = objectList.item(index2);
+
+                    if (object.getNodeType() == Node.ELEMENT_NODE) {
+                        Element objectElement = (Element) object;
+                        // get general Object Info everything has == name and type
+                        String objName = objectElement.getAttribute("name");
+                        String objType = objectElement.getAttribute("type");
+                        List<String> objectMethods = new ArrayList<String>();
+                        // if true -> complex object and not primitive var
+                        if (objectElement.hasAttribute("id")) {
+                            // get object specific info == anything but name and type
+                            String objectID = objectElement.getAttribute("id");
+                            // vars of that object
+                            NodeList attributeList = object.getChildNodes();
+
+                            Stack<String> attrIDs = new Stack<>();
+                            Stack<String> attrTypes = new Stack<>();
+                            Stack<String> attrValues = new Stack<>();
+
+                            for (int index3 = 0; index3 < attributeList.getLength(); index3++) {
+                                Node attribute = attributeList.item(index3);
+                                if (attribute.getNodeType() == Node.ELEMENT_NODE) {
+                                    Element attributeElement = (Element) attribute;
+
+                                    if (attributeElement.hasAttribute("type")) {
+                                        // get var info
+                                        String attrID = attributeElement.getAttribute("id");
+                                        String attrType = attributeElement.getAttribute("type");
+                                        String attrValue = attributeElement.getTextContent();
+                                        attrIDs.push(attrID);
+                                        attrTypes.push(attrType);
+                                        attrValues.push(attrValue);
+                                        if (connections.get(objName) == null) {
+                                            if (!isPrimitive(attrType))
+                                                if (attrValue.contains("[")) {
+                                                    String[] ids = attrValue.replace("[", "").replace("]", "")
+                                                            .split(" ");
+                                                    for (String id : ids) {
+                                                        String o = nameByID.get(id.trim());
+                                                        // check if null because of loop
+                                                        if (o != null)
+                                                            if (connections.get(objName) == null) {
+                                                                List<String> l = new ArrayList<String>();
+                                                                l.add(o);
+                                                                connections.put(objName, l);
+                                                            } else {
+                                                                List<String> l = connections.get(objName);
+                                                                if (!l.contains(o))
+                                                                    l.add(o);
+                                                                connections.put(objName, l);
+                                                            }
+                                                    }
+                                                } else {
+                                                    List<String> l = new ArrayList<String>();
+                                                    if (nameByID.get(attrValue) != null)
+                                                        l.add(nameByID.get(attrValue));
+                                                    connections.put(objName, l);
+                                                }
+                                        } else {
+                                            if (!isPrimitive(attrType))
+                                                if (attrValue.contains("[")) {
+                                                    String[] ids = attrValue.replace("[", "").replace("]", "")
+                                                            .split(" ");
+                                                    for (String id : ids) {
+                                                        String o = nameByID.get(id.trim());
+                                                        if (o != null) {
+                                                            List<String> l = connections.get(objName);
+                                                            if (!l.contains(o))
+                                                                l.add(o);
+                                                            connections.put(objName, l);
+                                                        }
+                                                    }
+                                                } else {
+                                                    List<String> l = connections.get(objName);
+                                                    if (nameByID.get(attrValue) != null)
+                                                        if (!l.contains(nameByID.get(attrValue)))
+                                                            l.add(nameByID.get(attrValue));
+                                                    connections.put(objName, l);
+                                                }
+                                        }
+                                    } else if (attributeElement.hasAttribute("args")) {
+                                        String methodID = attributeElement.getAttribute("id");
+                                        String methodArgs = attributeElement.getAttribute("args");
+                                        String methodRetType = attributeElement.getTextContent();
+
+                                        objectMethods.add(methodID + "(" + methodArgs + ")" + " : " + methodRetType);
+                                    }
+
+                                }
+                            }
+                            String[] methodArray = new String[objectMethods.size()];
+                            for (int i = 0; i < objectMethods.size(); i++) {
+                                methodArray[i] = objectMethods.get(i);
+                            }
+                            gc.createDotNode(objName + "Node");
+                            gc.createDotObject(objName, objectID, objType, attrIDs, attrTypes, attrValues,
+                                    methodArray);
+                            gc.connectDotNodeToObject(objName + "Node", objName);
+                        } else { // -> primitive var
+                            // get primitive var specific info == only value
+                            String objValue = objectElement.getTextContent();
+                            gc.createDotNode(objName + "Node");
+                            gc.createPrimitiveDotObject(objName, objType, objValue);
+                            gc.connectDotNodeToObject(objName + "Node", objName);
+                        }
+                    }
+
+                }
+            }
+            // connect the nodes referring to eachother
+            for (String key : connections.keySet()) {
+                List<String> ends = connections.get(key);
+                for (String s : ends) {
+                    if (s != null)
+                        if (connections.get(s.trim()) != null && connections.get(s.trim()).contains(key)) {
+                            connections.get(s.trim()).remove(key);
+                            gc.connectDotObjectToObjects("b", key, s.trim());
+                        } else
+                            gc.connectDotObjectToObjects("f", key, s.trim());
+                }
+
+            }
+
+            stepsDot[index1] = gc.getFinishedDotStep(index1);
+        }
+
+        // put the dot graphs in a txt file
+        //
+        // write the final html file
+        //part 1-3 because the strings are too big
+        FileWriter fw = new FileWriter(new File(dir + "ObjectDiagram.txt"));
+        for(int i = 0; i < stepsDot.length; i++) {
+            fw.write(stepsDot[i]);
+            fw.flush();
+        }
+        fw.close(); 
+    }
 
     // builds a xml file based of the previous created schemes, which are now getting filled
     // for an example looak at test.xml
